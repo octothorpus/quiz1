81658a090ac02043434f1328672d8b7f33df852f
octothorpus <Emily.LStevens@gmail.com>
2016-08-10 17:27:27 -0700

    Add files via upload
    this is a revision, now it works.

--- helper_methods.rb
+++ helper_methods.rb
@@ -2,29 +2,18 @@
 # 7. Implement the following code in Ruby: Create a module called HelperMethods that include a method called `titleize` that does the following: it takes in a string and returns the string back after capitalizing each word in that string. For example if you pass to the method a string "hello world" you should get back "Hello World". The methods should not capitalize the following words: in, the, of, and, or, from.
 # 11. BONUS: Rewrite the method `titleize` in question #7 without mutating any variable.
 #
-# print "Enter book title: "
-# sentence = gets.chomp
+print "Enter book title: "
+string = gets.chomp
 
 def titleize(string)
+  excluded_words = ["and", "the" ,"in" , "of", "or", "from"]
   array =[]
-  array << string.split.each {|x| x.capitalize! if x != "and" or x != "the" or x != "in" or x != "of" or x != "or" or x != "from"}
+  array << string.split.each { |word| word.capitalize! unless
+    excluded_words.include?(word) }
+  #  word.!include?("and", "the" ,"in" , "of", "or", "from")}
   array.join(" ")
 end
 
-# not done, this doesn't work.
 
-
-print titleize("where in the world are you from?!")
-
-
-
-#   # check for user input of exit to break loop, otherwise
-#   # shovel input into array
-#   name == "exit" ? break : books << name
-# end
 #
-# # for each input, split into words and capitalize
-# # however, do a regex and check if each word matches
-# # and/or/if etc. and skip if it does
-# # afterwords, rejoin the string together
-# puts books.sort.map { |x| x.split(" ").map { |y| y.match(/\bfor\b/i) ? y : y.capitalize }.join(" ") }
+print titleize(string)
